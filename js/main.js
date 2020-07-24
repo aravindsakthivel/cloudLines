9613c119538f5675fc1a16d494f97a6768575c45
AravindSakthivel <aravind.sakthi@outlook.com>
2020-07-24 19:58:54 +0530

    Added: Database to store registered user

---
+++ js/main.js
@@ -0,0 +1,62 @@
+class CreateRegData {
+    constructor(name){
+        this.name = name
+
+        this.init = () => {
+            let result = JSON.parse(localStorage.getItem(this.name))
+            if(!result){
+                this.updateDB([])
+            }
+        }
+
+        this.updateDB = (data) => {
+            localStorage.setItem(this.name, JSON.stringify(data))
+        }
+
+        this.allData = () => {
+            this.init()
+            return JSON.parse(localStorage.getItem(this.name))
+        }
+
+    }
+
+
+    addUser(userCreds){
+        let allUser = this.allData()
+        if(allUser.length === 0){
+            userCreds['id'] = allUser.length + 1
+            userCreds['flag'] = true
+        }
+        else{
+            userCreds['id'] = allUser.length + 1
+            userCreds['flag'] = true
+        }
+        allUser.push(userCreds)
+        this.updateDB(allUser)
+    }
+
+}
+
+
+
+class CurrentUser extends CreateRegData{
+    constructor(name){
+        super(name)
+        this.init = () =>{
+            let result = JSON.parse(localStorage.getItem(this.name))
+            if(!result){
+                this.updateDB({})
+            }
+        }
+    }
+
+
+    userWho(data){
+        this.updateDB(data)
+    }
+
+}
+
+
+let regUsers = new CreateRegData('Registered_Users')
+let lgdUser = new CurrentUser('Current_User')
